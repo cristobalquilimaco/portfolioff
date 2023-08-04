ad87e08c9c4cb5fb589978a3dcd22acca1cafb7d
cristobalquilimaco <quilimacox1@gmail.com>
2023-08-04 12:31:52 -0500

    se ajusta formulario responsive en css

--- src/components/Contactame.jsx
+++ src/components/Contactame.jsx
@@ -9,13 +9,13 @@ const Contactame = () => {
             <h1 className='about__ttt'>CONTACTA<span className='color_tt'>ME</span></h1>
  <h2 className='subtitle'>Aqui te dejo mis medios de contacto para que hablemos</h2>
             </div>
-            <section className=''>
+            <section className='contacme_with_form'>
       
                 <ul>
-  <li><i className='bx bxl-whatsapp'></i></li>
-  <li><i className='bx bx-envelope' ></i></li>
-  <li><i className='bx bxl-instagram' ></i></li>
-  <li><i className='bx bx-world' ></i></li>
+  <li><i className='bx bxl-whatsapp'></i>Este es mi whatsapp</li>
+  <li><i className='bx bx-envelope' ></i>Este es Correo</li>
+  <li><i className='bx bxl-instagram' ></i>Este es mi instagram</li>
+  <li><i className='bx bx-world' ></i>Este es netlify</li>
                 </ul>
                 <form className='form_contact' action="">
                 <ul className='list_form'>
