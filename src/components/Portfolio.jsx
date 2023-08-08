14defd3d676736e236211d7000b9f0019c367ec3
cristobalquilimaco <quilimacox1@gmail.com>
2023-08-08 18:05:28 -0500

    Se ajusta pantalla de la seccion portafolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -14,12 +14,12 @@ const Portfolio = () => {
           <p className='info_page1'>Proyecto E-commerce</p>
           <p className='description_project'>E-commerce hecha consumiendo la Api del instituto academlo</p>
           <div className='text_portfolio portfolio1'>
-            <a className='t_info' href="https://bejewelled-buttercream-3faeba.netlify.app/">E-commerce</a>
+            <a className='t_info' href="https://bejewelled-buttercream-3faeba.netlify.app/"><i className='bx bx-link-external'></i>E-commerce</a>
             <p className='tecnology'>Tecnologias</p>
             <article className='tec_pp'>
-              <p className='tec_info'>HTML</p>
-              <p className='tec_info'>JAVASCRIPT</p>
-              <p className='tec_info'>CSS</p>
+              <p className='tec_info'>html</p>
+              <p className='tec_info'>javaScript</p>
+              <p className='tec_info'>css</p>
             </article>
           </div>
           <img className='image_portfolio' src={images.ecommerceVanilla} alt="" />
@@ -28,7 +28,7 @@ const Portfolio = () => {
           <p className='info_page1'>Proyecto Pokedex</p>
           <p className='description_project'>Se consume API publica de pokemon para hacer una aplicacion tipo catalogo con todos los pokemons</p>
           <div className='text_portfolio portfolio1'>
-            <a className='t_info' href="https://pokedexcristobal.netlify.app/">Pokedex</a>
+            <a className='t_info' href="https://pokedexcristobal.netlify.app/"> <i className='bx bx-link-external'></i>Pokedex</a>
             <p className='tecnology'>Tecnologias</p>
             <article className='tec_pp'>
               <p className='tec_info'>React</p>
@@ -42,7 +42,7 @@ const Portfolio = () => {
           <p className='info_page1'>Proyecto Rick and Morty</p>
           <p className='description_project'>Se consume Api de Rick and Morty para obtener una lista con los personajes de la serie</p>
           <div className='text_portfolio portfolio1'>
-            <a className='t_info' href="https://createrickandmortyapp.netlify.app/">Rick And Morty</a>
+            <a className='t_info' href="https://createrickandmortyapp.netlify.app/"><i className='bx bx-link-external'></i>Rick And Morty</a>
             <p className='tecnology'>Tecnologias</p>
             <article className='tec_pp'>
               <p className='tec_info'>React</p>
@@ -56,7 +56,7 @@ const Portfolio = () => {
           <p className='info_page1'>Aplicacion del tiempo</p>
           <p className='description_project'>Se crea aplicacion para conocer el estado del tiempoen cualquier parte del mundo</p>
           <div className='text_portfolio portfolio1'>
-            <a className='t_info' href="https://openweatherappreactjs.netlify.app/">Weather App</a>
+            <a className='t_info' href="https://openweatherappreactjs.netlify.app/"><i className='bx bx-link-external'></i>Weather App</a>
             <p className='tecnology'>Tecnologias</p>
             <article className='tec_pp'>
               <p className='tec_info'>React</p>
