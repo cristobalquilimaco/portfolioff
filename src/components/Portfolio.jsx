1c064f0a3ca00abbc974f9c9a26f0473a859eb83
cristobalquilimaco <quilimacox1@gmail.com>
2023-10-21 09:54:09 -0500

    Se agreaga arreglo para mejor manejo de los proyectos del portafolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import "../components/styles/portfolio.css"
-import images from '../assets/images'
+import React from 'react';
+import "../components/styles/portfolio.css";
+import projects from '../utils/projects';
+
 
 const Portfolio = () => {
   return (
@@ -10,96 +11,25 @@ const Portfolio = () => {
         <p className='description_portfolio'>Échale un vistazo a mis proyectos más recientes.</p>
       </article>
       <section className='projects_name'>
-
-      <div className='section_project'>
-          <p className='info_page1'>E-commerce React</p>
-          <p className='description_project'>Se crean página web tipo tienda virtual de productos electrónicos.</p>
-          <div className='text_portfolio portfolio1'>
-            <a className='t_info' href="https://ecommercewithreactq.netlify.app/"><i className='bx bx-link-external'></i>E-commerce</a>
-            <p className='tecnology'>Tecnologias</p>
-            <article className='tec_pp'>
-              <p className='tec_info'>React</p>
-              <p className='tec_info'>Redux</p>
-              <p className='tec_info'>Router</p>
-            </article>
+        {projects.map((project) => (
+          <div key={project.id} className='section_project'>
+            <p className='info_page1'>{project.nombre}</p>
+            <p className='description_project'>{project.descripcion}</p>
+            <div className='text_portfolio portfolio1'>
+              <a className='t_info' href={project.url}><i className='bx bx-link-external'></i>E-commerce</a>
+              <p className='tecnology'>Tecnologias</p>
+              <article className='tec_pp'>
+                {project.tecnologias.map((tecnologia, index) => (
+                  <p key={index} className='tec_info'>{tecnologia}</p>
+                ))}
+              </article>
+            </div>
+            <img className='image_portfolio' src={project.imagen} alt="" />
           </div>
-          <img className='image_portfolio' src={images.ecommercereact} alt="" />
-        </div>
-
-        <div className='section_project'>
-          <p className='info_page1'>Proyecto E-commerce</p>
-          <p className='description_project'>E-commerce hecha consumiendo la Api del instituto academlo</p>
-          <div className='text_portfolio portfolio1'>
-            <a className='t_info' href="https://bejewelled-buttercream-3faeba.netlify.app/"><i className='bx bx-link-external'></i>E-commerce</a>
-            <p className='tecnology'>Tecnologias</p>
-            <article className='tec_pp'>
-              <p className='tec_info'>html</p>
-              <p className='tec_info'>javaScript</p>
-              <p className='tec_info'>css</p>
-            </article>
-          </div>
-          <img className='image_portfolio' src={images.ecommerceVanilla} alt="" />
-        </div>
-        <div className='section_project'>
-          <p className='info_page1'>Proyecto Pokedex</p>
-          <p className='description_project'>Se consume API de pokemon para hacer una aplicacion tipo catalogo con todos los pokemons</p>
-          <div className='text_portfolio portfolio1'>
-            <a className='t_info' href="https://pokedexcristobal.netlify.app/"> <i className='bx bx-link-external'></i>Pokedex</a>
-            <p className='tecnology'>Tecnologias</p>
-            <article className='tec_pp'>
-              <p className='tec_info'>React</p>
-              <p className='tec_info'>Redux</p>
-              <p className='tec_info'>Router</p>
-            </article>
-          </div>
-          <img className='image_portfolio' src={images.pokedex} alt="" />
-        </div>
-        <div className='section_project'>
-          <p className='info_page1'>Proyecto Rick and Morty</p>
-          <p className='description_project'>Se consume Api de Rick and Morty para obtener una lista con los personajes de la serie</p>
-          <div className='text_portfolio portfolio1'>
-            <a className='t_info' href="https://createrickandmortyapp.netlify.app/"><i className='bx bx-link-external'></i>Rick And Morty</a>
-            <p className='tecnology'>Tecnologias</p>
-            <article className='tec_pp'>
-              <p className='tec_info'>React</p>
-              <p className='tec_info'>Redux</p>
-              <p className='tec_info'>Router</p>
-            </article>
-          </div>
-          <img className='image_portfolio' src={images.rickandMorty} alt="" />
-        </div>
-        <div className='section_project'>
-          <p className='info_page1'>Aplicacion del tiempo</p>
-          <p className='description_project'>Se crea aplicacion para conocer el estado del tiempoen cualquier parte del mundo</p>
-          <div className='text_portfolio portfolio1'>
-            <a className='t_info' href="https://openweatherappreactjs.netlify.app/"><i className='bx bx-link-external'></i>Weather App</a>
-            <p className='tecnology'>Tecnologias</p>
-            <article className='tec_pp'>
-              <p className='tec_info'>React</p>
-              <p className='tec_info'>Redux</p>
-              <p className='tec_info'>Router</p>
-            </article>
-          </div>
-          <img className='image_portfolio' src={images.weatherapp} alt="" />
-        </div>
-        <div className='section_project'>
-          <p className='info_page1'>Juego tic tac toe</p>
-          <p className='description_project'>Se crea famoso juego tic tac toe, también conocido como la vieja.</p>
-          <div className='text_portfolio portfolio1'>
-            <a className='t_info' href="https://tictactoepractice.netlify.app/"><i className='bx bx-link-external'></i>Tic Tac Toe</a>
-            <p className='tecnology'>Tecnologias</p>
-            <article className='tec_pp'>
-              <p className='tec_info'>React</p>
-              <p className='tec_info'>JavaScript</p>
-              <p className='tec_info'>CSS</p>
-              <p className='tec_info'>HTML</p>
-            </article>
-          </div>
-          <img className='image_portfolio' src={images.tictactoe} alt="" />
-        </div>
+        ))}
       </section>
     </div>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
---
+++ src/utils/projects.js
@@ -0,0 +1,54 @@
+import images from "../assets/images";
+
+const projects = [
+    {
+      id: 1,
+      nombre: "E-commerce React",
+      descripcion: "Se crean página web tipo tienda virtual de productos electrónicos.",
+      tecnologias: ["React", "Redux", "Router"],
+      url: "https://ecommercewithreactq.netlify.app/",
+      imagen: images.ecommercereact,
+    },
+    {
+      id: 2,
+      nombre: "Proyecto E-commerce",
+      descripcion: "E-commerce hecha consumiendo la Api del instituto academlo",
+      tecnologias: ["HTML", "JavaScript", "CSS"],
+      url: "https://bejewelled-buttercream-3faeba.netlify.app/",
+      imagen: images.ecommerceVanilla,
+    },
+    {
+      id: 3,
+      nombre: "Proyecto Pokedex",
+      descripcion: "Se consume API de pokemon para hacer una aplicacion tipo catalogo con todos los pokemons",
+      tecnologias: ["React", "Redux", "Router"],
+      url: "https://pokedexcristobal.netlify.app/",
+      imagen: images.pokedex,
+    },
+    {
+      id: 4,
+      nombre: "Proyecto Rick and Morty",
+      descripcion: "Se consume Api de Rick and Morty para obtener una lista con los personajes de la serie",
+      tecnologias: ["React", "Redux", "Router"],
+      url: "https://createrickandmortyapp.netlify.app/",
+      imagen: images.rickandMorty,
+    },
+    {
+      id: 5,
+      nombre: "Aplicacion del tiempo",
+      descripcion: "Se crea aplicacion para conocer el estado del tiempo en cualquier parte del mundo",
+      tecnologias: ["React", "Redux", "Router"],
+      url: "https://openweatherappreactjs.netlify.app/",
+      imagen: images.weatherapp,
+    },
+    {
+      id: 6,
+      nombre: "Juego tic tac toe",
+      descripcion: "Se crea famoso juego tic tac toe, también conocido como la vieja.",
+      tecnologias: ["React", "JavaScript", "CSS", "HTML"],
+      url: "https://tictactoepractice.netlify.app/",
+      imagen: images.tictactoe,
+    },
+  ];
+  
+  export default projects;
